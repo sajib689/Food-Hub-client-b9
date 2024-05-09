22c242758215be4299a5a6c11efe0935383f7acb
sajib689 <jibonraja11@gmail.com>
2024-05-09 21:55:28 +0600

    add footer

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -71,25 +71,31 @@ const Navbar = () => {
                 href="#"
                 className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
-                Join Slack
+               Home
               </a>
               <a
                 href="#"
                 className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
-                Browse Topics
+                Available Foods
               </a>
               <a
                 href="#"
                 className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
-                Random Item
+               Add Food
               </a>
               <a
                 href="#"
                 className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
-                Experts
+                Manage My Foods
+              </a>
+              <a
+                href="#"
+                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+               My Food Request
               </a>
             </div>
 
