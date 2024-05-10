a771d776c6552418ee03db6a578c8deee21c775d
sajib689 <jibonraja11@gmail.com>
2024-05-10 21:11:48 +0600

    add delete btn

--- src/Components/ManageFoods.jsx
+++ src/Components/ManageFoods.jsx
@@ -4,26 +4,23 @@ import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import ManageFoodsCard from "./ManageFoodsCard";
 
-
 const ManageFoods = () => {
-    const {user} = useContext(AuthContext)
-    const { isPending, data,refetch } = useQuery({
-        queryKey: ['data'],
-        queryFn: () =>
-          fetch(`http://localhost:3000/foods?email=${user?.email}`)
-        .then((res) =>
-            res.json(),
-          ),
-      }
-    )
-    if (isPending) {
-        return <Loader/>
-      }
-    console.log(data)
-   
-    return (
-        <div className="container p-2 mx-auto sm:p-4 dark:text-gray-800">
-      <h2 className="mb-4 text-2xl font-semibold leading-tight">My List</h2>
+  const { user } = useContext(AuthContext);
+  const { isPending, data, refetch } = useQuery({
+    queryKey: ["data"],
+    queryFn: () =>
+      fetch(`http://localhost:3000/foods?email=${user?.email}`).then((res) =>
+        res.json()
+      ),
+  });
+  if (isPending) {
+    return <Loader />;
+  }
+ 
+
+  return (
+    <div className="container max-w-6xl p-2 mx-auto sm:p-4 dark:text-gray-800">
+      <h2 className="mb-4 text-2xl font-semibold leading-tight">My Food List</h2>
       <div className="overflow-x-auto">
         <table className="min-w-full text-xs">
           <colgroup>
@@ -36,12 +33,11 @@ const ManageFoods = () => {
           </colgroup>
           <thead className="dark:bg-gray-300">
             <tr className="text-left">
-              <th className="p-3">Country Name</th>
-              <th className="p-3">Tourist Spot Name</th>
-              <th className="p-3">Location</th>
-              <th className="p-3">Travel Time</th>
-              <th className="p-3">Total Visitors</th>
-              <th className="p-3">Average Cost</th>
+              <th className="p-3">Food Name</th>
+              <th className="p-3">Donator Name</th>
+              <th className="p-3">Donator Email</th>
+              <th className="p-3">Quantity</th>
+              <th className="p-3">Pickup Location</th>
               <th className="p-3">Delete</th>
               <th className="p-3">Update</th>
             </tr>
@@ -58,7 +54,7 @@ const ManageFoods = () => {
         </table>
       </div>
     </div>
-    );
+  );
 };
 
 export default ManageFoods;
--- src/Components/ManageFoodsCard.jsx
+++ src/Components/ManageFoodsCard.jsx
@@ -1,10 +1,40 @@
+import { Link } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import axios from "axios";
+AOS.init();
 
-
-const ManageFoodsCard = () => {
+const ManageFoodsCard = ({food}) => {
+    const {_id,foodName,donatorName,donatorEmail,quantity,pickupLocation,} = food
+    const handleDelete = (_id) => {
+        axios.delete()
+    };
     return (
-        <div>
-            
-        </div>
+        <tr className="border-b border-opacity-20 dark:border-gray-300 dark:bg-gray-50">
+        <td className="p-3">
+          <p>{foodName}</p>
+        </td>
+        <td className="p-3">
+          <p>{donatorName}</p>
+        </td>
+        <td className="p-3">
+          <p>{donatorEmail}</p>
+        </td>
+        <td className="p-3">
+          <p>{quantity} person</p>
+        </td>
+        <td className="p-3 ">
+          <p>{pickupLocation}</p>
+        </td>
+       
+        <td className="p-3 ">
+          <button onClick={() => handleDelete(_id)} className="btn bg-warning text-white">Delete</button>
+          
+        </td>
+        <td className="p-3">
+        <Link to={`/update/${_id}`} className="btn bg-success text-white">Update</Link>
+        </td>
+      </tr>
     );
 };
 
