ec3a2ea4507735ebb1cc71d758b518276b906e5e
sajib689 <jibonraja11@gmail.com>
2024-05-10 19:22:46 +0600

    add food add filed

--- src/Components/AddFood.jsx
+++ src/Components/AddFood.jsx
@@ -58,164 +58,107 @@ const AddFood = () => {
   return (
     <section
       data-aos="fade-down-right"
-      className="p-6 dark:bg-gray-100 dark:text-gray-900"
+      className="max-w-6xl mx-auto p-6 dark:bg-gray-100 dark:text-gray-900"
     >
       <form onSubmit={handlePostSpot} className="container mx-auto space-y-8">
-        <fieldset className="p-6 rounded-lg shadow-md ">
+        <fieldset className="p-6 rounded-lg shadow-md">
           <legend className="text-lg font-medium text-gray-800 dark:text-gray-200">
-            Tourist Spot Information
+            Food Information
           </legend>
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
             <div className="space-y-2">
               <label
-                htmlFor="tourist_spot_name"
+                htmlFor="food_image"
                 className="block text-sm font-medium text-gray-700 dark:text-gray-300"
               >
-                Tourist Spot Name
+                Food Image
               </label>
               <input
-                id="tourist_spot_name"
-                name="tourist_spot_name"
+                id="food_image"
+                name="food_image"
                 type="text"
-                placeholder="Tourist Spot Name"
+                placeholder="Food Image URL"
                 className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
               />
             </div>
             <div className="space-y-2">
               <label
-                htmlFor="country_name"
+                htmlFor="food_name"
                 className="block text-sm font-medium text-gray-700 dark:text-gray-300"
               >
-                Country
-              </label>
-              <select name="country_name" className="select select-bordered w-full max-w-xs">
-                <option disabled selected>
-                  Select Country
-                </option>
-                <option value='Bangladesh'>Bangladesh</option>
-                <option value='Thailand'>Thailand</option>
-                <option value='Indonesia'>Indonesia</option>
-                <option value='Malaysia'>Malaysia</option>
-                <option value='Vietnam'>Vietnam</option>
-                <option value='Cambodia'>Cambodia</option>
-              </select>
-              {/* <input
-                id="country_name"
-                name="country_name"
-                type="text"
-                placeholder="Country"
-                className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
-              /> */}
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="location"
-                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-              >
-                Location
+                Food Name
               </label>
               <input
-                id="location"
-                name="location"
+                id="food_name"
+                name="food_name"
                 type="text"
-                placeholder="Location"
+                placeholder="Food Name"
                 className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
               />
             </div>
             <div className="space-y-2">
               <label
-                htmlFor="short_description"
+                htmlFor="quantity"
                 className="block text-sm font-medium text-gray-700 dark:text-gray-300"
               >
-                Short Description
+                Quantity
+              </label>
+              <input
+                id="quantity"
+                name="quantity"
+                type="text"
+                placeholder="Quantity"
+                className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
+              />
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="pickup_location"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >
+                Pickup Location
+              </label>
+              <input
+                id="pickup_location"
+                name="pickup_location"
+                type="text"
+                placeholder="Pickup Location"
+                className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
+              />
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="expired_date_time"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >
+                Expired Date and Time
+              </label>
+              <input
+                id="expired_date_time"
+                name="expired_date_time"
+                type="datetime-local"
+                className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
+              />
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="additional_notes"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >
+                Additional Notes
               </label>
               <textarea
-                id="short_description"
-                name="short_description"
-                placeholder="Short Description"
+                id="additional_notes"
+                name="additional_notes"
+                placeholder="Additional Notes"
                 className="w-full h-20 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
               ></textarea>
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="average_cost"
-                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-              >
-                Average Cost
-              </label>
-              <input
-                id="average_cost"
-                name="average_cost"
-                type="text"
-                placeholder="Average Cost"
-                className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
-              />
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="seasonality"
-                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-              >
-                Seasonality
-              </label>
-              <input
-                id="seasonality"
-                name="seasonality"
-                type="text"
-                placeholder="Seasonality"
-                className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
-              />
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="travel_time"
-                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-              >
-                Travel Time
-              </label>
-              <input
-                id="travel_time"
-                name="travel_time"
-                type="text"
-                placeholder="Travel Time"
-                className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
-              />
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="total_visitors_per_year"
-                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-              >
-                Total Visitors Per Year
-              </label>
-              <input
-                id="total_visitors_per_year"
-                name="total_visitors_per_year"
-                type="text"
-                placeholder="Total Visitors Per Year"
-                className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
-              />
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="photoURL"
-                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-              >
-                Photo URL
-              </label>
-              <input
-                id="photoURL"
-                name="image"
-                type="text"
-                placeholder="Photo URL"
-                className="w-full h-10 rounded-md border-gray-300 focus:border-violet-600 focus:ring focus:ring-violet-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-700"
-              />
             </div>
           </div>
           <div className="mt-6">
             <button
               type="submit"
-              className="w-full py-2 bg-[#fc5a34] text-white rounded-md hover:bg-[#fc5a34] focus:outline-none focus:ring focus:ring-violet-600 focus:ring-opacity-50"
+              className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-violet-600 focus:ring-opacity-50"
             >
               Add
             </button>
