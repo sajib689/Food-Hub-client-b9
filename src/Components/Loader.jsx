7b33a5bccec391076de45672d9b2f97d9ceb85e2
sajib689 <jibonraja11@gmail.com>
2024-05-10 12:28:21 +0600

    add loader

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,18 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <MagnifyingGlass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="magnifying-glass-loading"
+        wrapperStyle={{}}
+        wrapperClass="magnifying-glass-wrapper"
+        glassColor="#c0efff"
+        color="#e15b64"
+      />
+    </div>
+  );
+};
+
+export default Loader;
