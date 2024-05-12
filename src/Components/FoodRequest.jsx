0dccbc0e96c0fcd893d0b292f0d22c3a26ceeea2
sajib689 <jibonraja11@gmail.com>
2024-05-12 15:38:33 +0600

    card done

--- src/Components/FoodRequest.jsx
+++ src/Components/FoodRequest.jsx
@@ -19,7 +19,7 @@ const FoodRequest = () => {
   return (
     <div className="container max-w-6xl p-2 mx-auto sm:p-4 dark:text-gray-800">
       <h2 className="mb-4 text-2xl font-semibold leading-tight">
-        My Food List
+        My Food Request
       </h2>
       <div className="overflow-x-auto">
         <table className="min-w-full text-xs">
@@ -33,13 +33,10 @@ const FoodRequest = () => {
           </colgroup>
           <thead className="dark:bg-gray-300">
             <tr className="text-left">
-              <th className="p-3">Food Name</th>
-              <th className="p-3">Donator Name</th>
-              <th className="p-3">Donator Email</th>
-              <th className="p-3">Quantity</th>
+              <th className="p-3">Donar Name</th>
               <th className="p-3">Pickup Location</th>
-              <th className="p-3">Delete</th>
-              <th className="p-3">Update</th>
+              <th className="p-3">Expire Date</th>
+              <th className="p-3">Request Date</th>
             </tr>
           </thead>
           <tbody>
--- src/Components/FoodRequestCard.jsx
+++ src/Components/FoodRequestCard.jsx
@@ -1,10 +1,24 @@
 
 
-const FoodRequestCard = () => {
+
+const FoodRequestCard = ({food}) => {
+    const {donatorName,pickupLocation,expiredDateTime,requestDate} = food
     return (
-        <div>
-            
-        </div>
+        <tr className="border-b border-opacity-20 dark:border-gray-300 dark:bg-gray-50">
+        <td className="p-3">
+          <p>{donatorName}</p>
+        </td>
+        <td className="p-3">
+          <p>{pickupLocation}</p>
+        </td>
+        <td className="p-3">
+          <p>{expiredDateTime}</p>
+        </td>
+        <td className="p-3">
+          <p>{requestDate} </p>
+        </td>
+       
+      </tr>
     );
 };
 
