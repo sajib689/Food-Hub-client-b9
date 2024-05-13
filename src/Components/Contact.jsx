54395905b6d4b5b71ebd653aaecf45e2d148abf7
sajib689 <jibonraja11@gmail.com>
2024-05-13 22:14:00 +0600

    change the contact image

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,13 +1,21 @@
 import Lottie from "lottie-react";
 import contactImg from "../../public/dd.json";
 import { useContext } from "react";
-import { AuthContext } from './../Provider/AuthProvider';
+import { AuthContext } from "./../Provider/AuthProvider";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 const Contact = () => {
-    const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   return (
     <div className="grid mt-24 mb-12 max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
       <div className="flex flex-col justify-between">
-        <div className="space-y-2">
+        <div
+          data-aos="fade-right"
+          data-aos-offset="300"
+          data-aos-easing="ease-in-sine"
+          className="space-y-2"
+        >
           <h2 className="text-4xl font-bold leading-tight lg:text-5xl">
             Let's talk!
           </h2>
@@ -16,12 +24,20 @@ const Contact = () => {
           </div>
         </div>
         <Lottie
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
           className="md:h-[60vh]"
           animationData={contactImg}
           loop={true}
         />
       </div>
-      <form noValidate="" className="space-y-6">
+      <form
+        data-aos="fade-down"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+        noValidate=""
+        className="space-y-6"
+      >
         <div>
           <label htmlFor="name" className="text-sm">
             Full name
@@ -53,22 +69,21 @@ const Contact = () => {
             className="w-full p-3 rounded dark:bg-gray-100"
           ></textarea>
         </div>
-       {
-        user ?
-        <button
-        type="submit"
-        className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-blue-500 text-white dark:bg-blue-500 dark:text-gray-50"
-      >
-        Send Message
-      </button>
-      :
-      <button
-      type="submit"
-      className="disabled w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-blue-500 text-white dark:bg-blue-500 dark:text-gray-50"
-    >
-      Send Message
-    </button>
-       }
+        {user ? (
+          <button
+            type="submit"
+            className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-blue-500 text-white dark:bg-blue-500 dark:text-gray-50"
+          >
+            Send Message
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="disabled w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-blue-500 text-white dark:bg-blue-500 dark:text-gray-50"
+          >
+            Send Message
+          </button>
+        )}
       </form>
     </div>
   );
