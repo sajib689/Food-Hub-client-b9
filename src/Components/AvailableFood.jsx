ac70cbecb2857abdf6d0ff070a6409170524ebde
sajib689 <jibonraja11@gmail.com>
2024-05-13 12:26:26 +0600

    search system add success

--- src/Components/AvailableFood.jsx
+++ src/Components/AvailableFood.jsx
@@ -1,49 +1,75 @@
 import { useQuery } from "@tanstack/react-query";
 import FeaturedCard from "./FeaturedCard";
-import Loader from './Loader';
+import Loader from "./Loader";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
-
+import axios from "axios";
+import { useState } from "react";
 
 const AvailableFood = () => {
-  const axiosSecure = useAxiosSecure()
-    const { isPending, data } = useQuery({
-        queryKey: ['data'],
-        queryFn: async () =>{
-          try{
-            const res = await axiosSecure.get(`/foods`)
-            return res.data
-          } catch (err){
-            if(err) {
-              console.log(err)
-            }
-          }
+  const axiosSecure = useAxiosSecure();
+  const [query, setQuery] = useState('')
+  const [searchResult, setSearchResult] = useState([])
+  const [layout, setLayout] = useState(3)
+  const { isPending, data } = useQuery({
+    queryKey: ["data"],
+    queryFn: async () => {
+      try {
+        const res = await axiosSecure.get(`/foods`);
+        return res.data;
+      } catch (err) {
+        if (err) {
+          console.log(err);
         }
-      })
-      
-      if (isPending) return <Loader/>
-    
+      }
+    },
+  });
+  const handleSearch = async() => {
+   try{
+    const res = await axiosSecure.get(`/foods?foodName=${query}`)
+    setSearchResult(res.data)
+   }catch(err){
+    console.log(err);
+   }
+  }
+  if (isPending) return <Loader />;
 
-    const foodsFilter = data.filter(food => food.status === 'available');
-  
-  
-    return (
-        <div>
-        <div className="container max-w-6xl mb-3  px-6 py-10 mx-auto">
-          <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">explore our  Available Foods</h1>
-          <div className="mt-2">
-            <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
-            <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
-            <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
-          </div>
+  const foodsFilter = data.filter((food) => food.status === "available");
 
+  return (
+    <div>
+      <div className="container max-w-6xl mb-3  px-6 py-10 mx-auto">
+        <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+          explore our Available Foods
+        </h1>
+        <div className="mt-2">
+          <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+          <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+          <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
         </div>
-        <div className="max-w-6xl mb-24 px-10 py-10 mx-auto grid grid-cols lg:grid-cols-3 md:grid-cols-3 gap-6">
-            {
-                foodsFilter.map(food => <FeaturedCard key={food._id} food={food}></FeaturedCard>)
-            }
+        {/* search filed add */}
+        <div className="w-[300px] mt-4">
+          <label className="  flex items-center gap-2">
+            <input value={query} onChange={(e) => setQuery(e.target.value)} type="text" className="grow focus:outline-none" placeholder="Search" />
+            <button onClick={handleSearch} className="btn bg-blue-500 text-white hover:bg-blue-600">
+            Search
+            </button>
+          </label>
         </div>
+        {/* end searchFiled */}
       </div>
-    );
+      <div className="max-w-6xl mb-24 px-10 py-10 mx-auto grid grid-cols lg:grid-cols-3 md:grid-cols-3 gap-6">
+      {searchResult.length > 0 ? (
+          searchResult.map((food) => (
+            <FeaturedCard key={food._id} food={food}></FeaturedCard>
+          ))
+        ) : (
+          foodsFilter.map((food) => (
+            <FeaturedCard key={food._id} food={food}></FeaturedCard>
+          ))
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AvailableFood;
